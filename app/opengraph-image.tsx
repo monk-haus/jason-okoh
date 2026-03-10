9803dada26d6c97247a84210d9872d07524d9133
monk-haus <hello@monk.haus>
2026-03-10 03:20:28 +0200

    fix

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -2,6 +2,8 @@ import { ImageResponse } from "next/og";
 import { readFileSync } from "fs";
 import { join } from "path";
 
+export const dynamic = "force-static";
+
 export const size = {
   width: 1200,
   height: 630,
